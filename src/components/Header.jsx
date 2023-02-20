f30f1c4c30ba8a566270582ddead7988e54a7532
feliciofilipe <filipe.felicio.1999@gmail.com>
2023-02-20 02:50:25 +0000

    Update pelomundo link

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -122,8 +122,8 @@ function MobileNavigation(props) {
             <nav className="mt-6">
               <ul className="-my-2 divide-y divide-zinc-100 text-base text-zinc-800 dark:divide-zinc-100/5 dark:text-zinc-300">
                 <MobileNavItem href="/about">Professor</MobileNavItem>
-                <MobileNavItem href="/about">Horário</MobileNavItem>
-                <MobileNavItem href="https://focus.jiujitsubraga.com/">Pelo Mundo</MobileNavItem>
+                <MobileNavItem href="/schedule">Horário</MobileNavItem>
+                <MobileNavItem href="https://pelomundo.jiujitsubraga.com/">Pelo Mundo</MobileNavItem>
                 {/* <MobileNavItem href="/projects">Projects</MobileNavItem> */}
                 {/* <MobileNavItem href="/speaking">Speaking</MobileNavItem> */}
                 {/* <MobileNavItem href="/uses">Uses</MobileNavItem> */}
@@ -165,7 +165,7 @@ function DesktopNavigation(props) {
       <ul className="flex rounded-full bg-white/90 px-3 text-sm font-medium text-zinc-800 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-white/10">
         <NavItem href="/about">Professor</NavItem>
         <NavItem href="/schedule">Horário</NavItem>
-        <NavItem href="https://focus.jiujitsubraga.com/">Pelo Mundo</NavItem>
+        <NavItem href="https://pelomundo.jiujitsubraga.com/">Pelo Mundo</NavItem>
       </ul>
     </nav>
   )
